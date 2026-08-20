a7e179110eeffb662c8390dbdbf372f4f9781ac6
Diogo Souza <diogo.souza@msn.com>
2026-08-19 21:39:10 -0300

    fix(release): exibir versão/build dos metadados do fyne package (ldflags só valem no build local)

--- ui/app.go
+++ ui/app.go
@@ -1,6 +1,8 @@
 package ui
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 
@@ -20,6 +22,14 @@ const (
 func Run(useCase inbound.PackageUseCase, version, build string) {
 	a := app.NewWithID(appID)
 	a.Settings().SetTheme(&uitheme.DarkTheme{})
+
+	// Binários gerados pelo `fyne package` carregam os metadados do
+	// FyneApp.toml (ID preenchido); eles têm precedência sobre os valores
+	// injetados via -ldflags, usados apenas em builds locais (make build).
+	if m := a.Metadata(); m.ID != "" {
+		version = m.Version
+		build = fmt.Sprintf("%05d", m.Build)
+	}
 
 	w := a.NewWindow(appTitle)
 	w.Resize(fyne.NewSize(820, 560))
